f7feb0b1ac283effeb87f2338d94d7a158abfdaa
Jacopo Marrone <jacopo.marrone27@gmail.com>
2022-09-14 20:20:24 +0200

    feat(footer): added "resume" link and fixed css

--- views/Home/assets/social-links.ts
+++ views/Home/assets/social-links.ts
@@ -1,7 +1,8 @@
 export const links = [
+  { label: 'Resume', permalink: 'https://docs.google.com/document/d/1NGgg6yUhbJWqH_CtOKlghFmaeRiB7wuvgbo28-nPr1c/edit?usp=sharing' },
   { label: 'CodeSandbox', permalink: 'https://codesandbox.io/u/tresorama' },
+  { label: 'CodePen', permalink: 'https://codepen.io/tresorama' },
   { label: 'Github', permalink: 'https://github.com/tresorama' },
   { label: 'BitBucket', permalink: 'https://bitbucket.org/tresorama/' },
-  { label: 'CodePen', permalink: 'https://codepen.io/tresorama' }
 ];
 export const portfolio_permalink = "https://github.com/tresorama/jacopomarrone";
