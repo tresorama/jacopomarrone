11dd07cad3313e39072b95741b38206b8e1b5931
Jacopo Marrone <jacopo.marrone27@gmail.com>
2022-12-22 09:53:05 +0100

    feat: reordered footer links

--- src/views/Home/assets/social-links.ts
+++ src/views/Home/assets/social-links.ts
@@ -1,8 +1,8 @@
 export const links = [
   { label: 'Resume', permalink: 'https://docs.google.com/document/d/1NGgg6yUhbJWqH_CtOKlghFmaeRiB7wuvgbo28-nPr1c/edit?usp=sharing' },
+  { label: 'Github', permalink: 'https://github.com/tresorama' },
   { label: 'CodeSandbox', permalink: 'https://codesandbox.io/u/tresorama' },
   { label: 'CodePen', permalink: 'https://codepen.io/tresorama' },
-  { label: 'Github', permalink: 'https://github.com/tresorama' },
   { label: 'BitBucket', permalink: 'https://bitbucket.org/tresorama/' },
 ];
 export const portfolio_permalink = "https://github.com/tresorama/jacopomarrone";
