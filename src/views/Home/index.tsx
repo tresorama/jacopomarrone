680b2510464479ceadcbbb16110d73fbcea8ef55
Jacopo Marrone <jacopo.marrone27@gmail.com>
2023-01-18 19:44:07 +0100

    feat: let control initial works open state by url query param

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HomeGrid } from './components/HomeGrid';
 import { ContactMe } from './components/ContactMe';
 import { Works } from './components/Works';
 import { Footer } from './components/Footer';
-
+import { useRouter } from 'next/router';
 
 export const HomeView = () => {
+  const router = useRouter();
   const [isVisibleWorks, setIsVisibleWorks] = React.useState(false);
   const [isVisibleContactMe, setIsVisibleContactMe] = React.useState(false);
+  useEffect(() => {
+    if (router.query["initial-is-visible-works"] === 'true') setIsVisibleWorks(true);
+  }, [router.query]);
   React.useEffect(() => {
     // Add classes, CSS need this
     document.querySelector('#__next')?.classList.add('home');
