66138809bcb616085463311a21a8b01129b70553
Jacopo Marrone <jacopo.marrone27@gmail.com>
2024-04-03 16:29:14 +0200

    fix: update `marked` usage to 5+ pattern

--- src/data/blog/utils/utils.markdown.ts
+++ src/data/blog/utils/utils.markdown.ts
@@ -1,4 +1,5 @@
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from "marked-highlight";
 import hljs from 'highlight.js';
 // NOTE: remember to include the highlight.js css theme, 
 // In Next.js you should include it in "_app.tsx" or in the component file
@@ -40,25 +41,27 @@ import hljs from 'highlight.js';
 // };
 
 export const compileMarkdownToHTMLString = (markdown: string) => {
-  marked.setOptions({
-    langPrefix: "hljs language-",
-    highlight: (code, lang) => {
-      // ensure a language is present
-      if (lang === "") lang = "plaintext";
-      // parse
-      try {
-        return hljs.highlight(code, { language: lang }).value;
-      } catch (error) {
-        console.log(`============== Error - parseMarkdownIntoHTMLString - START =================`);
-        console.log("input\n");
-        console.log(JSON.stringify({ lang, code }, null, 2));
-        console.log("\nerror\n");
-        console.error(error);
-        console.log(`============== Error - parseMarkdownIntoHTMLString  - END =================`);
-        return code;
+  const marked = new Marked(
+    markedHighlight({
+      langPrefix: "hljs language-",
+      highlight: (code, lang) => {
+        // ensure a language is present
+        if (lang === "") lang = "plaintext";
+        // parse
+        try {
+          return hljs.highlight(code, { language: lang }).value;
+        } catch (error) {
+          console.log(`============== Error - parseMarkdownIntoHTMLString - START =================`);
+          console.log("input\n");
+          console.log(JSON.stringify({ lang, code }, null, 2));
+          console.log("\nerror\n");
+          console.error(error);
+          console.log(`============== Error - parseMarkdownIntoHTMLString  - END =================`);
+          return code;
+        }
       }
-    }
-  });
-  const markdownAsHTMLString = marked(markdown);
+    })
+  );
+  const markdownAsHTMLString = marked.parse(markdown);
   return markdownAsHTMLString;
 };
