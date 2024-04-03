c03f34accfa3c4b08f1d24c5bf1530fc49e34693
Jacopo Marrone <jacopo.marrone27@gmail.com>
2024-04-03 03:08:01 +0200

    refactor: better constant handling

--- src/constants/shared.ts
+++ src/constants/shared.ts
@@ -1,4 +1,6 @@
 // Build Mode
+if (!process.env.NEXT_PUBLIC_BUILD_MODE) throw new Error('Missing env var => NEXT_PUBLIC_BUILD_MODE');
+
 export const IS_DEVELOPMENT = process.env.NEXT_PUBLIC_BUILD_MODE === 'development';
 export const IS_STAGING = process.env.NEXT_PUBLIC_BUILD_MODE === 'staging';
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_BUILD_MODE === 'production';
--- src/constants/utils/from-env.ts
+++ src/constants/utils/from-env.ts
@@ -1,8 +1,8 @@
-export const getFromEnv = <T extends string>(name: T) => {
+export const getFromEnv = <T extends (keyof NodeJS.ProcessEnv)>(name: T) => {
   console.log(`Checking existence of env var "${name}" ...`);
   const value = process.env[name];
   if (typeof value === 'undefined') {
-    throw new Error(`"${name}" is missing in process.env !`);
+    throw new Error(`Missing env var => "${name}" in process.env !`);
   }
   return value;
 };
