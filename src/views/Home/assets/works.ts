c8939a8349289677cb1a8c1962638ef5d77070a3
Jacopo Marrone <jacopo.marrone27@gmail.com>
2024-04-22 23:10:25 +0200

    refactor(types): add missing types for "works"

--- src/views/Home/assets/works.ts
+++ src/views/Home/assets/works.ts
@@ -1,58 +1,65 @@
-export const works = [
-  {
-    slug: 'regexer',
-    name: 'Regexer',
-    image: "/images/works-source/regexer.png",
-    caption: 'AI generator and tutor for regex',
-    description: `<p>A tool for developers.<br/><br/>You need a regex pattern but you always forget how regex works.<br /><br />With Regexer you can generate your regex asking in plain english.<br /><br />Or paste your regex and ask question about it.<br />Just ask!</p>`,
-    permalink: 'https://regexer.dev/'
-  },
-  {
-    slug: 'gradia',
-    name: 'Gradia',
-    image: "/images/works-source/gradia.png",
-    caption: 'Multi-Layer CSS gradient editor',
-    description: `<p>A tool for developers.<br/><br/>Be creative with your design and get the perfect background for your project. <br/><br />Stack multiple layer of linear or radial gradients with ease to use controls.<br /><br />When you are satisfied export to pure CSS (or CSS-in-JS).</p>`,
-    permalink: 'https://gradia.app/'
-  },
-  {
-    slug: 'pipocas',
-    name: 'Pipocas',
-    image: "/images/works/pipocas_02.jpg",
-    caption: 'Local SEO, landing page and sales dashboard',
-    description: `<p>Pipocas is an established vintage clothing shop in Reggio Emilia.</p><p>With my assistance, Pipocas was able to launch a successful online presence and increase potential customers in the local area.</p><p>I helped enhance customer experience by creating an easy-to-navigate landing page, enabling customers to browse their shop and contact them with ease. </p><p>Additionally, I developed an internal tool for increased business intelligence specifically tailored to their needs.</p>`,
-    permalink: 'https://www.pipocas.it/'
-  },
-  {
-    slug: 'galleria',
-    name: 'Galleria',
-    image: "/images/works/galleria.jpg",
-    caption: 'Photographer personal showreel',
-    description: '<p>Design gives value for what matter for a photographer, his/her photos.</p><p>Website act only as a background layer that doesn’t fight for user attention.</p><p>After user browsed the photographer works can easily contact the photographer via a form.</p><p class="secondary-text">This website is for demo purpose only.</p>',
-    permalink: 'https://funny-sunshine-fb735e.netlify.app/'
-  },
-  {
-    slug: 'color-compare',
-    name: 'Color Compare',
-    image: "/images/works/color-compare.jpg",
-    caption: 'Tool for comparing colors',
-    description: '<p>A tool for who works everyday with design and color codes ( RGB, HEX, HSL )</p><p>Useful when you is need to lay down two or more colors next to each other to easily find the “sweet” combination spot. </p><p> Nothing less nothing more.</p>',
-    permalink: 'https://color-compare.netlify.app/'
-  },
-  {
-    slug: 'fli',
-    name: 'Fli',
-    image: "/images/works/fli.jpg",
-    caption: 'Artist artwork presentation',
-    description: '<p>Cinematic sequence landing page used to create hype on product launch. </p><p>Fli wanted to give strength to his artwork presentation at an important artistic international event providing a countdown to the day of the event.</p>',
-    permalink: 'https://sad-einstein-7248c0.netlify.app/'
-  },
-  {
-    slug: 'smile',
-    name: 'Smile',
-    image: "/images/works/smile.jpg",
-    caption: 'Storytelling sequence',
-    description: '<p>"Smiling on a daily basis help humans stay healty".</p><p>This webpage aims to push the viewer to look for situations in everyday life that bring a smile to his or her face.</p>',
-    permalink: 'https://amazing-yonath-074bda.netlify.app/'
-  },
-];
+export const works: {
+  slug: string;
+  name: string;
+  image: string;
+  caption: string;
+  description: string;
+  permalink: string;
+}[] = [
+    {
+      slug: 'regexer',
+      name: 'Regexer',
+      image: "/images/works-source/regexer.png",
+      caption: 'AI generator and tutor for regex',
+      description: `<p>A tool for developers.<br/><br/>You need a regex pattern but you always forget how regex works.<br /><br />With Regexer you can generate your regex asking in plain english.<br /><br />Or paste your regex and ask question about it.<br />Just ask!</p>`,
+      permalink: 'https://regexer.dev/'
+    },
+    {
+      slug: 'gradia',
+      name: 'Gradia',
+      image: "/images/works-source/gradia.png",
+      caption: 'Multi-Layer CSS gradient editor',
+      description: `<p>A tool for developers.<br/><br/>Be creative with your design and get the perfect background for your project. <br/><br />Stack multiple layer of linear or radial gradients with ease to use controls.<br /><br />When you are satisfied export to pure CSS (or CSS-in-JS).</p>`,
+      permalink: 'https://gradia.app/'
+    },
+    {
+      slug: 'pipocas',
+      name: 'Pipocas',
+      image: "/images/works/pipocas_02.jpg",
+      caption: 'Local SEO, landing page and sales dashboard',
+      description: `<p>Pipocas is an established vintage clothing shop in Reggio Emilia.</p><p>With my assistance, Pipocas was able to launch a successful online presence and increase potential customers in the local area.</p><p>I helped enhance customer experience by creating an easy-to-navigate landing page, enabling customers to browse their shop and contact them with ease. </p><p>Additionally, I developed an internal tool for increased business intelligence specifically tailored to their needs.</p>`,
+      permalink: 'https://www.pipocas.it/'
+    },
+    {
+      slug: 'galleria',
+      name: 'Galleria',
+      image: "/images/works/galleria.jpg",
+      caption: 'Photographer personal showreel',
+      description: '<p>Design gives value for what matter for a photographer, his/her photos.</p><p>Website act only as a background layer that doesn’t fight for user attention.</p><p>After user browsed the photographer works can easily contact the photographer via a form.</p><p class="secondary-text">This website is for demo purpose only.</p>',
+      permalink: 'https://funny-sunshine-fb735e.netlify.app/'
+    },
+    {
+      slug: 'color-compare',
+      name: 'Color Compare',
+      image: "/images/works/color-compare.jpg",
+      caption: 'Tool for comparing colors',
+      description: '<p>A tool for who works everyday with design and color codes ( RGB, HEX, HSL )</p><p>Useful when you is need to lay down two or more colors next to each other to easily find the “sweet” combination spot. </p><p> Nothing less nothing more.</p>',
+      permalink: 'https://color-compare.netlify.app/'
+    },
+    {
+      slug: 'fli',
+      name: 'Fli',
+      image: "/images/works/fli.jpg",
+      caption: 'Artist artwork presentation',
+      description: '<p>Cinematic sequence landing page used to create hype on product launch. </p><p>Fli wanted to give strength to his artwork presentation at an important artistic international event providing a countdown to the day of the event.</p>',
+      permalink: 'https://sad-einstein-7248c0.netlify.app/'
+    },
+    {
+      slug: 'smile',
+      name: 'Smile',
+      image: "/images/works/smile.jpg",
+      caption: 'Storytelling sequence',
+      description: '<p>"Smiling on a daily basis help humans stay healty".</p><p>This webpage aims to push the viewer to look for situations in everyday life that bring a smile to his or her face.</p>',
+      permalink: 'https://amazing-yonath-074bda.netlify.app/'
+    },
+  ];
